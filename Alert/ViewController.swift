b059c2468efdec56155be1bca845d3d725115f6a
forthenew <forthenew@gmail.com>
2020-12-03 23:34:25 +0900

    초기 셋팅

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    let lampOn = UIImage(named: "lamp-on.png")
+    let lampOff = UIImage(named: "lamp-off.png")
+    let lampRemove = UIImage(named: "lamp-remove.png")
+    
+    var isLampOn: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = lampOn
     }
 
-
+    @IBAction func btnLampOn(_ sender: Any) {
+        let lampController = UIAlertController(title: "램프켜기", message: "램프를 켤까요?", preferredStyle: .alert)
+        let onAction = UIAlertAction(title: "네, 켭니다.", style: .default, handler: nil)
+        
+        lampController.addAction(onAction)
+        present(lampController, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnLampOff(_ sender: Any) {
+        let lampController = UIAlertController(title: "램프끄기", message: "램프를 끌까요?", preferredStyle: .alert)
+        let offAction = UIAlertAction(title: "네. 끕니다.", style: .default, handler: nil)
+        lampController.addAction(offAction)
+        
+        present(lampController, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnLampRemove(_ sender: Any) {
+        print("###btnLampRemove")
+        imageView.image = lampRemove
+    }
 }
 
